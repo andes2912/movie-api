02717ce323527e9616d40c096c4cff8d77baa9ed
Andri Desmana <andridesmana29@gmail.com>
2022-07-26 11:47:02 +0700

    Frontend

---
+++ Modules/Frontend/Services/FrontendService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Frontend\Services;
+
+use App\Helpers\ClientResponderHelper;
+use Exception;
+use Modules\Backend\Entities\Movie;
+
+class FrontendService {
+    use ClientResponderHelper;
+
+    // Discover Movie
+    public function Discover()
+    {
+        try {
+            $discover = Movie::paginate(10);
+            return $this->responseSuccess(200,'success', $discover);
+        } catch (\Exception $e) {
+            return $this->responseFailed($e->getMessage());
+        }
+    }
+
+    // Latest Movie
+    public function Latest()
+    {
+        try {
+            $latest = Movie::orderBy('created_at','desc')->paginate(10);
+            return $this->responseSuccess(200,'success', $latest);
+        } catch (\Exception $e) {
+            return $this->responseFailed($e->getMessage());
+        }
+    }
+
+    // Detail Movie
+    public function Detail($params)
+    {
+        try {
+            $slug = $params['slug'];
+            $detail = Movie::where('slug', $slug)->first();
+            if(!$detail) throw new Exception("Movie Not Found.");
+            return $this->responseSuccess(200,'success', $detail);
+        } catch (\Exception $e) {
+            return $this->responseFailed($e->getMessage());
+        }
+    }
+}
